d4c52037d444eddf94638d26329133aa41a6763d
Ezra Szanton <ezraszanton@yahoo.com>
2020-03-20 04:19:29 -0400

    Platformer code!
    Created a base for the player controller including some ~juicy~ stuff like coyote time and input buffering on jumps. Not super well documented yet but I'm planning on going back to comment things out :)

---
+++ Assets/Scripts/Player Logic/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jump_impulse;
+    public float speed;
+    public Rigidbody2D body;
+
+    public float jump_buffer;
+
+    public Transform right_foot;
+    public Transform left_foot;
+
+    float jump_expiration;
+    public LayerMask jumpable;
+
+    float coyote_end;
+    public float coyote_time = 0.1f;
+
+    float jump_end;
+    public float jump_time;
+    public float hang_time = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics2D.OverlapCircle(left_foot.position, 0.01f, jumpable) || Physics2D.OverlapCircle(right_foot.position, 0.01f, jumpable))
+        {
+            beginCoyote();
+        }
+        if (Input.GetAxis("Vertical") > 0 && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)))
+        {
+            jumpInput();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+
+        if ( (jumpIsBuffered() || Input.GetAxis("Vertical") == 1) && isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jump_impulse);
+            coyote_end = 0;
+            jump_end = jump_time + Time.realtimeSinceStartup;
+        } else if(Input.GetAxis("Vertical") > 0 && Time.realtimeSinceStartup < jump_end)
+        {
+            body.velocity = new Vector2(body.velocity.x, jump_impulse);
+        } else if(Input.GetAxis("Vertical") > 0 && Time.realtimeSinceStartup < jump_end + hang_time)
+        {
+            body.velocity = new Vector2(body.velocity.x, 0);
+        } else
+        {
+            body.velocity = new Vector2(body.velocity.x, Mathf.Min(body.velocity.y, 0));
+
+            jump_end = 0;
+            jump_expiration = 0;
+        }
+    }
+
+    void beginCoyote()
+    {
+        coyote_end = coyote_time + Time.realtimeSinceStartup;
+    }
+
+    bool isGrounded()
+    {
+        return Time.realtimeSinceStartup < coyote_end;
+    }
+
+    void jumpInput()
+    {
+        jump_expiration = jump_buffer + Time.realtimeSinceStartup;
+    }
+
+    bool jumpIsBuffered()
+    {
+        return Time.realtimeSinceStartup < jump_expiration;
+    }
+}
---
+++ Assets/Scripts/Player Logic/squash.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class squash : MonoBehaviour
+{
+    public Rigidbody2D body;
+
+    public Vector3 init_transform;
+    public Vector3 squish;
+    public Vector3 stretch;
+
+    public float y_threshold;
+    public float change_threshold;
+
+    //all in reference to just the y motion
+    float velocity_diff = 0;
+    float last_velocity = 0;
+    float curr_velocity;
+
+    public float elasticity;
+
+    public Vector3 target;
+
+    private void FixedUpdate()
+    {
+        curr_velocity = body.velocity.y;
+        velocity_diff = curr_velocity - last_velocity;
+        last_velocity = curr_velocity;
+
+        if(Mathf.Abs(curr_velocity) > y_threshold)
+        {
+            target = stretch;
+        } else if (velocity_diff > change_threshold)
+        {
+            transform.localScale = squish;
+        }
+        else
+        {
+           target = init_transform;
+        }
+
+        transform.localScale = Vector3.Lerp(transform.localScale, target, elasticity);
+    }
+}
