1f20d3b35163fbb5f7964e89578c65cd9bd78d3e
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-02 13:49:43 -0400

    hotfix
    ummm, the colliders had gravity. Turned that shit right off

--- Assets/Scripts/BreakOnCollide.cs
+++ Assets/Scripts/BreakOnCollide.cs
@@ -7,7 +7,7 @@ public class BreakOnCollide : MonoBehaviour
     public float BreakSpeed = 40;
     public Rigidbody2D body;
 
-    float speed_buffer = 0;
+    //float speed_buffer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +18,16 @@ public class BreakOnCollide : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(body.velocity.y) > BreakSpeed)
+/*        if (Mathf.Abs(body.velocity.y) > BreakSpeed)
         {
             speed_buffer = Time.realtimeSinceStartup + 0.1f;
-        }
+        }*/
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Breakable"))
         {
-            if(Time.realtimeSinceStartup < speed_buffer)
+            if(collision.relativeVelocity.sqrMagnitude > BreakSpeed * BreakSpeed)
             {
                 Destroy(collision.gameObject);
             }
