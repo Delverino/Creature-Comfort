cad8fb484f1187929b0b5203888cba912791cfd4
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-10 11:24:07 -0400

    door!
    Added a door prefab, doesn't have controller functionality yet but currently if you click the up arrow over it as mo you'll go to the specified level.

---
+++ Assets/door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class door : MonoBehaviour
+{
+    public int destinationScene;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.UpArrow))
+        {
+            SceneManager.LoadScene(destinationScene);
+        }
+    }
+}
