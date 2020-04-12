4f4561480a8202bb62c9e6788e23c77e3ed7a04f
rebecca-millerr <me@beccamiller.com>
2020-04-12 00:26:32 -0500

    lever and cage

---
+++ Assets/Scripts/CageOpen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageOpen : MonoBehaviour
+{
+    public Animator cageOpen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cageOpen.enabled = false;
+        this.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cageOpen.enabled) 
+        {
+            StartCoroutine(OpenCage());
+        }
+    }
+
+    IEnumerator OpenCage()
+    {
+        yield return new WaitForSeconds(1);
+        this.GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
--- Assets/Scripts/lever.cs
+++ Assets/Scripts/lever.cs
@@ -8,29 +8,59 @@ public class lever : MonoBehaviour
     public PlayerMovement animal;
     public SmartCamera cam;
     public TransformPlayer tp;
+    public Animator leverPull;
+    public CageOpen cage;
+    private bool triggerable;
 
     void Start()
     {
         animal.enabled = false;
+        triggerable = false;
+        leverPull.enabled = false;
     }
 
     void Update()
     {
+        // temporarily using E as key
+        if (triggerable && Input.GetKeyDown(KeyCode.E))
+        {
+            // leverPull.enabled = true;
+            StartCoroutine(TriggerAnimation());
+        }
+    }
+
+    IEnumerator TriggerAnimation()
+    {
+        leverPull.enabled = true;
+        yield return new WaitForSeconds(2);
         
+        BecomeAnimal();
+        cage.cageOpen.enabled = true;
+    }
+
+    private void BecomeAnimal()
+    {
+        mo.enabled = false;
+        mo.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        animal.enabled = true;
+        cam.Focus(animal.transform);
+        Destroy(gameObject);
+        tp.on = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            mo.enabled = false;
-            mo.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-            animal.enabled = true;
-            cam.Focus(animal.transform);
-            Destroy(gameObject);
-            tp.on = false;
+            triggerable = true;
         }
+    }
 
-
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            triggerable = false;
+        }
     }
 }
