b4816309477ee00262cabdbb81b3880a6dcb6ecb
U-DESKTOP-LGKG921\josef <josefine-tijssen@hotmail.com>
2020-04-14 07:01:11 -0400

    Pause menu, quit, toggle, and prefab

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+	public static bool isPaused = false;
+	public GameObject PMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+        	if(isPaused){
+        		Resume();
+        	}
+        	else{
+        		Pause();
+        	}
+        }
+    }
+    public void Resume(){
+    	PMenuUI.SetActive(false);
+    	Time.timeScale = 1f;
+    	isPaused = false;
+    }
+    void Pause(){
+    	PMenuUI.SetActive(true);
+    	Time.timeScale = 0f;
+    	isPaused = true;
+    }
+	public void ExitGame(){
+		Debug.Log("Exiting...");
+		Application.Quit();
+	}
+}
