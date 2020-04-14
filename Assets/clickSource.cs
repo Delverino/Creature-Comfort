7d5ac264e0694a0e8c5e4e9f88030df9611f217f
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-14 11:04:56 -0400

    clicks prefab and organized sound
    moved sounds around and added a "clicks" prefab which makes a clicking sound

---
+++ Assets/clickSource.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickSource : MonoBehaviour
+{
+    AudioSource clickBox;
+
+    public static clickSource click;
+
+    private void Awake()
+    {
+        clickBox = GetComponent<AudioSource>();
+        click = this;
+    }
+
+    void play()
+    {
+        clickBox.Play();
+    }
+}
