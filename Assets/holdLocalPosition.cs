a724a75a97e7dcbe68544d6720432045a94c8dd2
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-17 01:54:32 -0400

    Mo hands
    Added hand gameobjects and the scripts to make them work, one which turns them the right color and another which makes them try to follow an object (offset by their initial offset)

---
+++ Assets/holdLocalPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holdLocalPosition : MonoBehaviour
+{
+    public float response;
+
+    public Transform tether;
+
+    Vector3 homePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        homePos = transform.position - tether.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.localPosition = Vector3.Lerp(transform.position, tether.position + homePos, response);
+    }
+}
---
+++ Assets/imitateColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class imitateColor : MonoBehaviour
+{
+    public SpriteRenderer target;
+
+    SpriteRenderer sprite;
+
+    Color initColor;
+    Color invisible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        initColor = target.color;
+        invisible = initColor;
+        invisible.a = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(target.gameObject.activeInHierarchy == false)
+        {
+            sprite.color = invisible;
+        } else
+        {
+            sprite.color = initColor;
+        }
+    }
+}
