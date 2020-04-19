d265dbf47317ba1eb343f464a02ed81d17b5e680
Peter Lam <Peter.Lam@tufts.edu>
2020-04-19 18:33:22 -0400

    Removing sprites for slots that don't have animals yet
    Super buggy implementation (for example, the slot disappears but not the image inside it) so it needs further work

--- Assets/Scripts/Animal UI/AnimalPanel.cs
+++ Assets/Scripts/Animal UI/AnimalPanel.cs
@@ -8,6 +8,8 @@ public class AnimalPanel : MonoBehaviour
     public Canvas animalPanel;
     public TransformPlayer tp;
 
+    List<Button> slots = new List<Button>();
+
     public Button slot1;
     public Button slot2;
     public Button slot3;
@@ -17,65 +19,107 @@ public class AnimalPanel : MonoBehaviour
     public Sprite highlightedSlot;
     public Sprite unhighlightedSlot;
 
-    private int numAnimalsUnlocked;
+    // Invariant: will never be higher than numAnimals
+    private int currentlyHighlighted;
 
-    // Initially sets highlighted animalPanel to Mo--this can be
-    // changed/done better
-    // especially if a level starts with an animal that is not Mo. 
+    // Currently set automatically to be the number of animals in the
+    // Transformation manager
+    public int numAnimals;
+    
+
+    // Initially sets highlighted animalPanel to Mo--this might be better
+    // done by a public variable 
     void Start()
     {
         animalPanel = GetComponent<Canvas> ();
-        updatePanel(1);
-        numAnimalsUnlocked = tp.animals.Count;
+        
+        currentlyHighlighted = 0;
 
+        numAnimals = tp.animals.Count;
+
+        // Better way to do this: get buttons by getting children
+        // of parent object
+        slots.Add(slot1);
+        slots.Add(slot2);
+        slots.Add(slot3);
+        slots.Add(slot4);
+        slots.Add(slot5);
+
+        // Setting up slot images--set Mo's slot to be highlighted and every other
+        // slot not 
+        slots[0].GetComponent<Image>().sprite = highlightedSlot;
+        for (int i = 1; i < 5; i++)
+        {
+            if (i < numAnimals)
+            {
+                Debug.Log("Setting Slot " + i + " to be unhighlighted");
+                slots[i].GetComponent<Image>().sprite = unhighlightedSlot;
+            }
+            else
+            {
+                Debug.Log("Setting Slot " + i + " to be inactive");
+                slots[i].GetComponent<Image>().sprite = null;
+            }
+        }
     }
 
     // Updates which slot is highlighted based on which animal is selected
     // Also super unoptimized code--there's definitely a better way to do this
-    void updatePanel(int numAnimal) {
-        
-
-        switch (numAnimal)
+    void updatePanel(int highlighted) {
+        for (int i = 0; i < 5; i++)
         {
-            case 1:
-                
-                slot1.GetComponent<Image>().sprite = highlightedSlot;
-                slot2.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot3.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot4.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot5.GetComponent<Image>().sprite = unhighlightedSlot;
-                break;
-            case 2:
-                slot1.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot2.GetComponent<Image>().sprite = highlightedSlot;
-                slot3.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot4.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot5.GetComponent<Image>().sprite = unhighlightedSlot;
-                break;
-            case 3:
-                slot1.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot2.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot3.GetComponent<Image>().sprite = highlightedSlot;
-                slot4.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot5.GetComponent<Image>().sprite = unhighlightedSlot;
-                break;
-            case 4:
-                slot1.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot2.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot3.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot4.GetComponent<Image>().sprite = highlightedSlot;
-                slot5.GetComponent<Image>().sprite = unhighlightedSlot;
-                break;
-            case 5:
-                slot1.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot2.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot3.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot4.GetComponent<Image>().sprite = unhighlightedSlot;
-                slot5.GetComponent<Image>().sprite = highlightedSlot;
-                break;
-            default:
-                break;
+            if (i == highlighted)
+            {
+                slots[i].GetComponent<Image>().sprite = highlightedSlot;
+            } else if (i < numAnimals)
+            {
+                slots[i].GetComponent<Image>().sprite = unhighlightedSlot;
+            } else
+            {
+                slots[i].GetComponent<Image>().sprite = null;
+            }
         }
+        currentlyHighlighted = highlighted;
+        //switch (highlighted)
+        //{
+        //    case 1:
+        //        slot1.GetComponent<Image>().sprite = highlightedSlot;
+        //        slot2.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot3.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot4.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot5.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        break;
+        //    case 2:
+        //        slot1.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot2.GetComponent<Image>().sprite = highlightedSlot;
+        //        slot3.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot4.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot5.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        break;
+        //    case 3:
+        //        slot1.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot2.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot3.GetComponent<Image>().sprite = highlightedSlot;
+        //        slot4.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot5.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        break;
+        //    case 4:
+        //        slot1.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot2.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot3.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot4.GetComponent<Image>().sprite = highlightedSlot;
+        //        slot5.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        break;
+        //    case 5:
+        //        slot1.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot2.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot3.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot4.GetComponent<Image>().sprite = unhighlightedSlot;
+        //        slot5.GetComponent<Image>().sprite = highlightedSlot;
+        //        break;
+        //    default:
+        //        break;
+        //}
     }
 
     // Update is called once per frame
@@ -83,37 +127,39 @@ public class AnimalPanel : MonoBehaviour
     {
         // Not sure if this is being called too often/if it could slow down
         // the game
-        numAnimalsUnlocked = tp.animals.Count;
-
+        numAnimals = tp.animals.Count;
+        updatePanel(currentlyHighlighted);
         if (tp.on) 
         {
         var key = Input.inputString;
 
         if (key == "1") 
         {
+            Debug.Log("Setting Mo");
             tp.SetActiveAnimal(0);
             slot1.GetComponent<Image>().sprite = highlightedSlot;
-            updatePanel(1);
-            }
-        if (Input.GetKeyDown(KeyCode.Alpha2) && 2 <= numAnimalsUnlocked) 
+            updatePanel(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && 2 <= numAnimals)
         {
+            Debug.Log("Setting Kangaroo");
             tp.SetActiveAnimal(1);
-            updatePanel(2);
-            }
-        if (Input.GetKeyDown(KeyCode.Alpha3) && 3 <= numAnimalsUnlocked) 
+            updatePanel(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && 3 <= numAnimals) 
         {
             tp.SetActiveAnimal(2);
-            updatePanel(3);
+            updatePanel(2);
             }
-        if (Input.GetKeyDown(KeyCode.Alpha4) && 4 <= numAnimalsUnlocked) 
+        if (Input.GetKeyDown(KeyCode.Alpha4) && 4 <= numAnimals) 
         {
             tp.SetActiveAnimal(3);
-            updatePanel(4);
+            updatePanel(3);
             }
-        if (Input.GetKeyDown(KeyCode.Alpha5) && 5 <= numAnimalsUnlocked) 
+        if (Input.GetKeyDown(KeyCode.Alpha5) && 5 <= numAnimals) 
         {
             tp.SetActiveAnimal(4);
-            updatePanel(5);
+            updatePanel(4);
         }
     }
     }
