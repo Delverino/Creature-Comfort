d5664ea28588e65179ee5ee9cd084d732f11790c
rebecca-millerr <me@beccamiller.com>
2020-04-20 14:39:59 -0500

    buggy tooltips

---
+++ Assets/Scripts/Dialogue/ETooltip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ETooltip : MonoBehaviour
+{
+    public Canvas tooltip;
+
+    void Start()
+    {
+        Debug.Log("starting");
+        tooltip.GetComponent<Canvas>().enabled = false;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {   
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Animal")
+        {
+            Debug.Log("colliding");
+            tooltip.GetComponent<Canvas>().enabled = true;
+        } 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {   
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Animal")
+        {
+            Debug.Log("leaving collider");
+            tooltip.GetComponent<Canvas>().enabled = false;
+        } 
+    }
+
+}
