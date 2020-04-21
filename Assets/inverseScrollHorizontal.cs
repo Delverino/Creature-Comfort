81be0fd6e20e499d532ad20e3b288380fb35dd96
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-21 15:39:28 -0400

    parallax

---
+++ Assets/inverseScrollHorizontal.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inverseScrollHorizontal : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    public Camera cam;
+    Transform camTransform;
+
+    SpriteRenderer sprite;
+
+    Vector3 prevCamPos;
+    Vector3 currCamPos;
+    float xDiff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        camTransform = cam.gameObject.transform;
+        sprite = GetComponent<SpriteRenderer>();
+
+        prevCamPos = camTransform.position;
+        currCamPos = prevCamPos;
+
+        //RectTransform rt = (RectTransform)transform;
+        Debug.Log("image width: " + sprite.bounds.size.x/*rt.rect.width*/);
+        Debug.Log("image height: " + sprite.bounds.size.y);
+
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+
+        Debug.Log("camera width: " + width);
+        Debug.Log("camera height: " + height);
+    }
+
+    private void Update()
+    {
+        prevCamPos = currCamPos;
+        currCamPos = camTransform.position;
+
+        xDiff = currCamPos.x - prevCamPos.x;
+
+        transform.localPosition -= Vector3.right * xDiff * scrollSpeed;
+
+        
+    }
+}
---
+++ Assets/parallax.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    public Camera cam;
+    Transform camTransform;
+
+    SpriteRenderer sprite;
+
+    Vector3 prevCamPos;
+    Vector3 currCamPos;
+    float xDiff;
+
+    float spriteWidth;
+    float spriteHeight;
+    float camWidth;
+    float camHeight;
+
+    public List<GameObject> filler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTransform = cam.gameObject.transform;
+        sprite = filler[0].GetComponent<SpriteRenderer>();
+
+        prevCamPos = camTransform.position;
+        currCamPos = prevCamPos;
+
+        spriteWidth = sprite.bounds.size.x;
+        spriteHeight = sprite.bounds.size.y;
+
+        camHeight = 2f * cam.orthographicSize;
+        camWidth = camHeight * cam.aspect;
+    }
+
+    private void Update()
+    {
+        inverseScrollHorizontal();
+
+        Debug.Log("Inside left: " + insideLeft(filler[0]));
+        Debug.Log("Inside right: " + insideRight(filler[0]));
+        /*        if (offLeft())
+                {
+                    moveRight();
+                } else if (offRight())
+                {
+                    moveLeft();
+                }*/
+    }
+
+    #region old
+    
+        void inverseScrollHorizontal()
+        {
+            prevCamPos = currCamPos;
+            currCamPos = camTransform.position;
+
+            xDiff = currCamPos.x - prevCamPos.x;
+
+            transform.localPosition -= Vector3.right * xDiff * scrollSpeed;
+        }
+    
+        bool insideLeft(GameObject g)
+        {
+            float leftSprite = g.transform.position.x - (spriteWidth / 2);
+            float leftCam = camTransform.position.x - (camWidth / 2);
+            return leftSprite > leftCam;
+        }
+
+        bool insideRight(GameObject g)
+      {
+            float rightSprite = g.transform.position.x + (spriteWidth / 2);
+            float rightCam = camTransform.position.x + (camWidth / 2);
+            return rightSprite < rightCam;
+        }
+    /*
+        void moveRight()
+        {
+            float newX = camTransform.position.x + ((camWidth + spriteWidth) / 2);
+            Vector3 tempPos = transform.position;
+            tempPos.x = newX;
+            transform.position = tempPos;
+        }
+
+        void moveLeft()
+        {
+            float newX = camTransform.position.x - ((camWidth + spriteWidth) / 2);
+            Vector3 tempPos = transform.position;
+            tempPos.x = newX;
+            transform.position = tempPos;
+        }*/
+    #endregion
+}
