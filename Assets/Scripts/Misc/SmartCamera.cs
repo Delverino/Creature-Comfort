dc27968e7683cc6d426f9bfab2de550636f51ba7
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-25 12:08:06 -0400

    added camera bounds

--- Assets/Scripts/Misc/SmartCamera.cs
+++ Assets/Scripts/Misc/SmartCamera.cs
@@ -20,9 +20,6 @@ public class SmartCamera : MonoBehaviour
 
     public Camera cam;
 
-    [Tooltip("Default height of the camera")]
-    public float base_height;
-
     public List<view> views;
 
     private float zoom;
@@ -30,9 +27,36 @@ public class SmartCamera : MonoBehaviour
     float wait_time = 0.1f;
     float stop_waiting = 0;
 
+    public Transform bottomLeftBound;
+    public Transform topRightBound;
+
+    float camHeight;
+    float camWidth;
+
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
+
+    Vector3 newPos;
+
     // Start is called before the first frame update
     void Awake()
     {
+        if(bottomLeftBound.localPosition == topRightBound.localPosition)
+        {
+            Debug.LogError("Please set the bounds of the camera: " + gameObject);
+        }
+
+        camHeight = 2f * cam.orthographicSize;
+        camWidth = camHeight * cam.aspect;
+
+        minX = bottomLeftBound.position.x;
+        minY = bottomLeftBound.position.y;
+
+        maxX = topRightBound.position.x;
+        maxY = topRightBound.position.y;
+
         views = new List<view>();
         view main_view = new view();
         main_view.transform = main_target;
@@ -46,11 +70,21 @@ public class SmartCamera : MonoBehaviour
 
     private void FixedUpdate()
     {
+
         if (Time.realtimeSinceStartup > stop_waiting)
         {
             transform.position = Vector3.Lerp(transform.position, views[views.Count - 1].transform.position, response);
             transform.position = new Vector3(transform.position.x, transform.position.y, -10);
             cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, views[views.Count - 1].size, response);
+            
+            camHeight = 2f * cam.orthographicSize;
+            camWidth = camHeight * cam.aspect;
+
+            newPos.y = Mathf.Clamp(transform.position.y, minY + (camHeight / 2), maxY - (camHeight / 2));
+            newPos.x = Mathf.Clamp(transform.position.x, minX + (camWidth / 2), maxX - (camWidth / 2));
+            newPos.z = -10;
+
+            transform.position = newPos;
         }
     }
 
@@ -60,7 +94,7 @@ public class SmartCamera : MonoBehaviour
         new_view.transform = new_target;
         new_view.size = height / 2;
         views.Add(new_view);
-        Debug.Log(views.Count);
+        //Debug.Log(views.Count);
         stop_waiting = wait_time + Time.realtimeSinceStartup;
     }
 
@@ -70,13 +104,13 @@ public class SmartCamera : MonoBehaviour
         new_view.transform = new_target;
         new_view.size = views[0].size;
         views[0] = new_view;
-        Debug.Log("new character focus");
+        //Debug.Log("new character focus");
         stop_waiting = wait_time + Time.realtimeSinceStartup;
     }
 
     public void RemoveView(Transform t)
     {
-        Debug.Log("removing " + t);
+        //Debug.Log("removing " + t);
         for (int i = 0; i < views.Count; i++)
         {
             if(views[i].transform == t)
