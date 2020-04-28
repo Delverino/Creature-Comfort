dace7029aa67fd3515a0258208e45b3afe34925f
lwang0 <lwangzero@gmail.com>
2020-04-27 21:58:46 -0700

    made the free phone

---
+++ Assets/Scripts/Dialogue/DialogueContactCheck.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueContactCheck : MonoBehaviour
+{
+    public DialogueTrigger trigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !trigger.alreadyTriggered)
+        {
+            trigger.TriggerDialogue();
+        }
+    }
+}
